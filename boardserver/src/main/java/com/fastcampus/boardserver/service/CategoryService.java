c06ab3cade24460476450390effd788fb7c15a63
함감사 <gsham0331@dearu.com>
2024-05-09 16:16:04 +0900

    33

--- boardserver/src/main/java/com/fastcampus/boardserver/service/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.fastcampus.boardserver.service;
-
-import com.fastcampus.boardserver.dto.CategoryDTO;
-
-public interface CategoryService {
-    void register(String userId, CategoryDTO categoryDTO);
-    void update(CategoryDTO categoryDTO);
-    void delete(Long id);
-}
